ce93f07b544c07260504890fd3e65ee73a2ee056
BiagioCozzolino <biagiocozzolino1996@gmail.com>
2022-07-01 10:29:42 +0200

    Milestone 1 Completata

--- src/jana60/Evento.java
+++ src/jana60/Evento.java
@@ -1,6 +1,7 @@
 package jana60;
 
 import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
 
 public class Evento {
 
@@ -22,6 +23,35 @@ public class Evento {
 		this.validNumPostTotali();
 	}
 
+	// Formattatore data
+	DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd/MM/yyyy");
+
+	// Getter and Setters
+
+	public String getTitolo() {
+		return titolo;
+	}
+
+	public void setTitolo(String titolo) {
+		this.titolo = titolo;
+	}
+
+	public LocalDate getData() {
+		return data;
+	}
+
+	public void setData(LocalDate data) {
+		this.data = data;
+	}
+
+	public int getNumeroPostiTotali() {
+		return numeroPostiTotali;
+	}
+
+	public int getNumeroPostiPrenotati() {
+		return numeroPostiPrenotati;
+	}
+
 	// Metodi
 	public void validaData() throws Exception {
 		if (data.isBefore(LocalDate.now())) {
@@ -34,4 +64,39 @@ public class Evento {
 			throw new Exception("Puoi inserire solo un numero positivo come PostiTotali!, riprova");
 
 	}
+
+	public void prenota() throws Exception {
+		if (numeroPostiPrenotati == numeroPostiTotali) {
+			throw new Exception(
+					"Mi dispiace ma i posti disponibili per questo evento sono terminati. Scegli un altro evento!");
+
+		} else if (data.isBefore(LocalDate.now())) {
+			throw new Exception("Mi dispiace ma l'evento selezionato è già passato!");
+		} else {
+			numeroPostiPrenotati++;
+		}
+	}
+
+	public void disdici() throws Exception {
+		if (numeroPostiPrenotati == 0) {
+			throw new Exception(
+					"Non ci sono prenotazioni per questo evento, non è possibile disdire se non hai prenotato!");
+
+		} else if (data.isBefore(LocalDate.now())) {
+			throw new Exception("Mi dispiace ma l'evento selezionato è già passato, è troppo tardi per disdire!");
+		} else {
+			numeroPostiPrenotati--;
+		}
+	}
+
+	public String dataFormattata() {
+		return formatter.format(data);
+	}
+
+	@Override
+	public String toString() {
+		return "L'evento selezionato inizierà il giorno: " + dataFormattata() + ". Il titolo dell'evento è: " + titolo
+				+ ". Buon divertimento!";
+	}
+
 };
